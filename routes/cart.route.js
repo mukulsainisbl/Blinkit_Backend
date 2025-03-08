021443fcc518b2e0ef2f8da50f52463cf0c6cfad
mukulsaini <mukulsainisbl1234@gmail.com>
2025-03-08 20:41:55 +0530

    Add auth on Order

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -3,9 +3,9 @@ import auth from "../middleware/authMiddleware.js";
 import { addToCartItemController, deleteCartItemQtyController, getCartItemController, updateCartItemQtyController } from "../controllers/cart.controller.js";
 const cartRouter = Router()
 
-cartRouter.post('/create' ,  addToCartItemController)
-cartRouter.get('/get' ,  getCartItemController)
-cartRouter.put('/update-quantity' , updateCartItemQtyController)
-cartRouter.delete('/delete-cart-item' ,  deleteCartItemQtyController)
+cartRouter.post('/create' ,auth,  addToCartItemController)
+cartRouter.get('/get' , auth,  getCartItemController)
+cartRouter.put('/update-quantity' ,auth, updateCartItemQtyController)
+cartRouter.delete('/delete-cart-item' ,auth,  deleteCartItemQtyController)
 
 export default cartRouter
